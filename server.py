0982dd1f13bac39b6f285feafb96704c9197fb55
Cheos <cheoscrafter@gmail.com>
2024-10-28 20:37:49 +0100

    implement server logic

--- server.py
+++ server.py
@@ -5,53 +5,142 @@ from websockets.server import WebSocketServerProtocol as WSServer
 from websockets.frames import CloseCode
 from pathlib import Path
 from typing import Any
-
-"""
+from enum import Enum
 from random import randint, choice
 
-sol: int = randint(1, 3)
+# just realized i don't even need websockets.... could've done all this using plain old http, too
 
-i: int = int(input('Choose a gate (1,2,3): '))
+# client compare order: check if error, check if active, check values according to local state
 
-elim: int = choice([x for x in range(1, 4) if x not in [sol, i]])
+class GameState(Enum):
+    INIT = 1 # connection initiated, no action taken yet
+    SELECTED = 2 # user selected a gate
+    ELIMINATED = 3 # a non-selected and non-winning gate was eliminated
+    SWITCHED = 4 # the user chose to (or not to) switch their selection
+    REVEALED = 5 # the winning gate is revealed and the game is finished
 
-print(f'Gate {elim} was eliminated')
-
-choice: str = input('Do you want to change your selection? (y,n): ')
-
-if choice == 'y':
-    i = [x for x in range(1, 4) if x not in [elim, i]][0]
-
-print(f'Your choice (gate {i}) was {"correct" if i == sol else "wrong"}!')
-"""
+class UserState:
+    def __init__(self):
+        self.game_state: GameState = GameState.INIT
+        self.winning: int = randint(1, 3)
+        self.selection: int = 0
+        self.eliminated: int = 0
+        self.switched: bool = False
 
 
-
+active: bool = False
 admin_handles: list[WSServer] = []
 user_handles: dict[str, WSServer] = {}
+user_states = dict[str, UserState] = {}
+game_stats = { 'total': 0, 'won': 0, 'lost': 0, 'switched': 0, 'won_switched': 0, 'lost_switched': 0, 'stayed': 0, 'won_stayed': 0, 'lost_stayed': 0 }
 
-async def broadcast(wss: list[WSServer], msg: Any) -> None:
-    for ws in wss:
-        try:
-            await ws.send(json.dumps(msg))
-        except:
-            pass
+def decorate(data: dict[str, Any]) -> dict[str, Any]:
+    data['active'] = active
+    return data
+
+async def send(ws: WSServer, msg: Any) -> None:
+    await ws.send(json.dumps(msg))
+
 
 async def handle_admin(ws: WSServer) -> None:
+    global active
     admin_handles.append(ws)
 
     async for msg in ws:
-        pass
+        try:
+            data = json.loads(msg)
+            action = data['action']
+            match action:
+                case 'start':
+                    active = True
+                case 'query':
+                    await send(ws, decorate(game_stats))
+                case _:
+                    await send(ws, { 'error': f'invalid action: {action}'})
+        except:
+            await send(ws, { 'error': 'malformed data' })
     
     admin_handles.remove(ws)
 
 
 async def handle_user(ws: WSServer, id: str) -> None:
     user_handles[id] = ws
+    user_states[id] = state = UserState()
 
     async for msg in ws:
-        pass
+        try:
+            data = json.loads(msg)
+            action = data['action']
+
+            if not active:
+                await send(ws, decorate({}))
+                continue
+            
+            match action:
+                case 'is_active':
+                    await send(ws, decorate({}))
+
+                case 'select':
+                    if state.game_state not in [ GameState.INIT ]:
+                        await send(ws, { 'error': 'incorrect action order, follow: select, (then switch,) then reveal' })
+                        continue
+
+                    selection = data.get('selection', None)
+                    if not selection or 1 > selection or 3 < selection:
+                        await send(ws, { 'error': f'invalid "selection": {selection}, must be one of: [1, 2, 3]' })
+                        continue
+
+                    state.selection = selection
+                    state.game_state = GameState.SELECTED
+
+                    state.eliminated = choice([x for x in range(1, 4) if x not in [state.selection, state.winning]])
+                    state.game_state = GameState.ELIMINATED
+                    await send(ws, decorate({
+                        'selection': state.selection,
+                        'eliminated': state.eliminated
+                    }))
+
+                case 'switch':
+                    if state.game_state not in [ GameState.ELIMINATED ]:
+                        await send(ws, { 'error': 'incorrect action order, follow: select, (then switch,) then reveal' })
+                        continue
+                    state.switched = True
+                    state.game_state = GameState.SWITCHED
+
+                case 'reveal':
+                    if state.game_state not in [ GameState.SWITCHED, GameState.ELIMINATED ]:
+                        await send(ws, { 'error': 'incorrect action order, follow: select, (then switch,) then reveal' })
+                        continue
+
+                    won: bool = (state.selection == state.winning) ^ state.switched
+                    game_stats['total'] += 1
+                    game_stats['won' if won else 'lost'] += 1
+                    game_stats['switched' if state.switched else 'stayed'] += 1
+                    game_stats[('won' if won else 'lost') + ('_switched' if state.switched else '_stayed')] += 1
+
+                    state.game_state = GameState.REVEALED
+                    await send(ws, decorate({
+                        'selection': state.selection,
+                        'eliminated': state.eliminated,
+                        'switched': state.switched,
+                        'winning': state.winning,
+                        'won': (state.selection == state.winning) ^ state.switched # neq if switched, else eq
+                    }))
+
+                case 'reset':
+                    user_states[id] = state = UserState()
+                    await send(ws, decorate({}))
+
+                case 'stop':
+                    await send(ws, { 'action': 'stop' })
+                    break
+
+                case _:
+                    await send(ws, { 'error': f'invalid action: {action}'})
+        except:
+            await send(ws, { 'error': 'malformed data' })
     
+    del user_states[id]
     del user_handles[id]
 
 async def handle_ws(ws: WSServer, path: str) -> None:
@@ -60,7 +149,7 @@ async def handle_ws(ws: WSServer, path: str) -> None:
     path_parts = path.split('/')
 
     if len(path_parts) != 1:
-        await ws.send('{"error": "invalid path"}')
+        await send(ws, { 'error': 'invalid path' })
         await ws.close(CloseCode.GOING_AWAY, "invalid path")
         return
 
